4a92777356d007020fe9645175b922a14678cc4b
jorgeluismorales <53586348+jorgeluismorales@users.noreply.github.com>
2021-08-03 10:20:32 -0300

    Tarea 11: Tareas completadas y pendientes

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ tareas.cargarTareasFromArray(tareasDB);
       case "2":
         tareas.listadoCompleto();
         break;
+
+      case "3":
+        tareas.listarPendientesCompletadas();
+        break;
+      case "4":
+        tareas.listarPendientesCompletadas(false);
+        break;
     }
 
     guardarDB( tareas.listadoArr);
