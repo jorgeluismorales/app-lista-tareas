ed4f04b4dd7c09775b8909810965e11f2646d33b
jorgeluismorales <53586348+jorgeluismorales@users.noreply.github.com>
2021-08-03 10:41:36 -0300

    Tarea 12: Listado para borrar y confirmacion de borrado

--- models/tareas.js
+++ models/tareas.js
@@ -58,6 +58,12 @@ class Tareas {
         })
     }
 
+    borrarTarea (id) {
+        if (this._listado[id]) {
+            delete this._listado[id];
+        }
+    }
+
     crearTarea(desc = '') {
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
